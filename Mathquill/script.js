b5c481af086aa002ed84cdfdd335e1327d7a7376
mangchohyeon <qgeon0419@gmail.com>
2025-01-19 22:45:09 +0900

    pi값 구하는 식(미완성) 작업중

--- Mathquill/script.js
+++ Mathquill/script.js
@@ -1,25 +1,11 @@
-const span1 = document.getElementById("span1");
-
-function adjust_width(span) 
-{
-    span.style.width = ""; // 초기화
-    let scrollWidth = span.scrollWidth; // 내용의 실제 너비
-    if (scrollWidth == 0) 
-    {
-        scrollWidth = 10;
-    }
-    span.style.width = scrollWidth + "px"; // 동적으로 너비 설정
-}
-
-
-
-function formatMathExpression(expression) 
-{
-    return expression.replace(/\^([^\s]+)/g, "<sup>$1</sup>");
-}
-
-span1.addEventListener("input", () => {
-    const formattedContent = formatMathExpression(span1.innerText);
-    span1.innerHTML = formattedContent;
-    adjust_width(span1);
+window.addEventListener('DOMContentLoaded', () => {
+  var MQ = MathQuill.getInterface(2);
+  var formulaSpans = document.getElementsByClassName("span_formula");
+  Array.from(formulaSpans).forEach(span => {
+    var mathField = MQ.MathField(span, {
+      spaceBehavesLikeTab: true,
+    });
+    // 초기 수식 설정
+    mathField.latex('lim_{x \\to \\infty} \\sum_{n=1}^{\\infty} \\pi e');
+  });
 });
